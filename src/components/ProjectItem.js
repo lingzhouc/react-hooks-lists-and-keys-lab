3772df795cadea3d091d21df4e8a94ee94d99117
LingZhouChen <lingzhouc@gmail.com>
2023-10-04 12:32:48 -0400

    complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  // create span tag for each tech
+  const techItems = technologies.map((tech) => {
+    return <span key={tech}>{tech}</span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techItems}
       </div>
     </div>
   );
